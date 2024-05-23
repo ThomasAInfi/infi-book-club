2ee4884a37d18ae01d06f40891ecfd34fce80d79
Thomas Abbink <thomas.abbink@infi.nl>
2024-05-23 15:59:38 +0200

    added github deploy action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
     footer: {
       style: "dark",
       links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Infi. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
